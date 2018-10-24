c24f5585cbb3d0572555133089c3ddb6edf8b69a
Morten Hartvigsen <m.hartvigsen@gmail.com>
2018-10-24 13:16:51 +0200

    Removed uneccessary code

--- code/src/VideoCapture/Webcam.java
+++
@@ -1,9 +0,0 @@
-package VideoCapture;
-
-import java.io.IOException;
-
-public interface Webcam {
-  byte[] getFrame();
-  int nextFrame() throws IOException;
-  void close() throws IOException;
-}
--- code/src/runner.java
+++ code/src/runner.java
@@ -1,26 +1,9 @@
-import VideoCapture.VideoCapture;
-import java.io.IOException;
-import lejos.hardware.BrickFinder;
 import lejos.hardware.Button;
-import lejos.hardware.lcd.Font;
-import lejos.hardware.lcd.GraphicsLCD;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
-import lejos.hardware.video.YUYVImage;
-import lejos.robotics.RegulatedMotor;
 
 public class runner {
   public static void main(String[] args) {
-    try {
-      VideoCapture webcam = new VideoCapture(160, 120);
-
-      YUYVImage img = new YUYVImage(webcam.getFrame(), webcam.getWidth(), webcam.getHeight());
-
-      GraphicsLCD g = BrickFinder.getDefault().getGraphicsLCD();
-      final int SW = g.getWidth();
-      final int SH = g.getHeight();
-      g.setFont(Font.getSmallFont());
-
       EV3LargeRegulatedMotor motor1 = new EV3LargeRegulatedMotor(MotorPort.A);
       EV3LargeRegulatedMotor motor2 = new EV3LargeRegulatedMotor(MotorPort.B);
 
@@ -34,16 +17,7 @@ public class runner {
       motor1.endSynchronization();
 
       while (!Button.ESCAPE.isDown()) {
-        webcam.nextFrame();
-        img.display(g, 0, 0, 150);
+
       }
-      webcam.close();
-      g.clear();
-    }catch (IOException ioe) {
-      ioe.printStackTrace();
-      System.out.println("Driver exception: " + ioe.getMessage());
-    }
   }
-
-
 }
