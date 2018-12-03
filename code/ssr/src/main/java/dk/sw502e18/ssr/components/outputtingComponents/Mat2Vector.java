48ab5efef9900eda93de996a29e36f320abbec6d
Mikkel <mikkel.xzx@gmail.com>
2018-12-03 10:38:19 +0100

    Removed output element, now waiting for refactored structure

--- code/ssr/src/main/java/dk/sw502e18/ssr/components/outputtingComponents/Mat2Vector.java
+++ code/ssr/src/main/java/dk/sw502e18/ssr/components/outputtingComponents/Mat2Vector.java
@@ -3,44 +3,23 @@ package dk.sw502e18.ssr.components.outputtingComponents;
 import dk.sw502e18.ssr.pipeline.Step;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfInt;
-import org.opencv.imgcodecs.Imgcodecs;
-
+import org.opencv.core.MatOfFloat;
 import java.io.*;
-import java.util.Arrays;
 
 public class Mat2Vector implements Step<Mat, Mat> {
 
-    private File path;
-    private int counter = 0;
+    private static float normalizeValue = 255.0f;
 
-    public Mat2Vector(String input) {
-        File path = new File(input);
-        if (!path.exists()){
-            try {
-                path.createNewFile();
-            }catch (IOException e){
-                throw new RuntimeException("Unable to create file");
-            }
-        }
-        if (path.canWrite()) {
-            this.path = path;
-        } else {
-            throw new RuntimeException("OutPutter unable to write to output-path.");
-        }
-    }
 
     private void output(Mat input){
-        MatOfInt rgb = new MatOfInt(CvType.CV_32S);
-        input.convertTo(rgb, CvType.CV_32S);
-        int size = (int) rgb.total() * rgb.channels();
-        int[] data = new int[size];
-        rgb.get(0,0, data);
-        try{
-            BufferedWriter bw = new BufferedWriter(new FileWriter(path.getAbsolutePath()));
-            bw.write(Arrays.toString(data));
-        } catch (IOException e) {
-            throw new RuntimeException(e.getMessage());
+        MatOfFloat floatImage = new MatOfFloat (CvType.CV_32F);
+        input.convertTo(floatImage, CvType.CV_32F);
+        int size = (int) floatImage.total() * floatImage.channels();
+        float[] data = new float[size];
+        floatImage.get(0,0, data);
+        for (int i = 0; i < data.length; i++) {
+            if (data[i]>=normalizeValue) { data[i] = 1; }
+            else data[i] = data[i]/normalizeValue;
         }
     }
 
