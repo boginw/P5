b1fe425bbc78094cfc08beefecf7a1cc73a74535
MalteZA <mzan16@student.aau.dk>
2018-12-12 11:04:04 +0100

    Flatten circle (#70)
    * How about now? Yes?
    
    * Inital draft is found in FlattenCircleInitDraft.md
    
    * Anybody asked for flattened circles, yes?
    
    * Smthng extra.
    
    * The flattening seems to be happening.
    
    * corrected threshold name
    
    * How you like me now?
    
    * Delete draft from code-branch.
    
    * Fixed what should be fixed.

--- code/ssr/src/main/java/dk/sw502e18/ssr/CircleCropper.java
+++ code/ssr/src/main/java/dk/sw502e18/ssr/CircleCropper.java
@@ -1,5 +1,7 @@
 package dk.sw502e18.ssr;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.opencv.core.*;
 import org.opencv.imgproc.Imgproc;
 
@@ -26,8 +28,11 @@ public class CircleCropper {
 
         RotatedRect ellipse = Imgproc.fitEllipse(points);
 
+        // Circle flattening is attempted. Reverts back to normal if flattening fails.
+        Pair<Mat, RotatedRect> flattenedAttempt = flattenCircle(src, dst, ellipse);
+
         try {
-            return cropEllipse(dst, ellipse);
+            return cropEllipse(flattenedAttempt.getLeft(), flattenedAttempt.getRight());
         } catch (IndexOutOfBoundsException e) {
             return null;
         }
@@ -120,6 +125,84 @@ public class CircleCropper {
         Rect rect = Imgproc.boundingRect(contours.get(0));
 
         return masked.submat(rect);
+    }
+
+    /**
+     * Attempts to warp/transform / "flatten" ellipse into circle.
+     *
+     * @param inputSrc Orignal red-channel Mat containing ellipse to be flattened.
+     * @param inputDst Original picture containing red ellipse (to be flattened).
+     * @param ellipse  The original ellipse.
+     * @return New flattened picture and corresponding ellipse, or original picture and ellipse if flattening failed.
+     */
+    private Pair<Mat, RotatedRect> flattenCircle(Mat inputSrc, Mat inputDst, RotatedRect ellipse) {
+
+        // Temporary placeholders used to preserve original inputsSrc and inputDst.
+        Mat tempDstMat1 = new Mat();
+        Mat tempDstMat2 = new Mat();
+        Mat tempSrcMat1 = new Mat();
+        Mat tempSrcMat2 = new Mat();
+        // Temporary Mat used with various opencv-methods
+        MatOfPoint2f tempMatOP2f;
+
+        // Inputs is rotated to align ellipse focal-points with x/y-axis. Stored in temp-Mats1.
+        Mat rotateM = Imgproc.getRotationMatrix2D(ellipse.center, ellipse.angle, 1.0);
+        Imgproc.warpAffine(inputDst, tempDstMat1, rotateM, new Size(tempDstMat1.width(), tempDstMat1.width()));
+        Imgproc.warpAffine(inputSrc, tempSrcMat1, rotateM, new Size(tempSrcMat1.width(), tempSrcMat1.width()));
+
+        // Left/right top/bottom points are located on ellipse.
+        int x = (int)ellipse.center.x;
+        int y = (int)ellipse.center.y;
+        MatOfPoint2f perspectInputMat;
+        Point p3 = findEdge(tempSrcMat1, x, y, 0, -1, threshold); // Left-most
+        Point p4 = findEdge(tempSrcMat1, x, y, 0, 1, threshold);  // Right-most
+        Point p1 = findEdge(tempSrcMat1, x, y, -1, 0, threshold); // Bottom
+        Point p2 = findEdge(tempSrcMat1, x, y, 1, 0, threshold);  // Top
+
+        // Storing p1-p4 as matrix.
+        try {
+            perspectInputMat = new MatOfPoint2f(p1, p2, p3, p4);
+        } catch (NullPointerException e) {
+            return new ImmutablePair<>(inputSrc, ellipse);
+        }
+
+        // Transformation matrix for ellipse-to-circle is calculated.
+        Mat perspectTransfMat = Imgproc.getPerspectiveTransform(
+                perspectInputMat,  // 1st arg: Points from before.
+                new MatOfPoint2f( // 2nd arg: Points to "land" on.
+                        new Point(x - (y - p3.y), p1.y),
+                        new Point(x + (y - p3.y), p2.y),
+                        p3,
+                        p4
+                )
+        );
+
+        // Image warp - ellipse becomes circle, result stored in temp-Mats2
+        Imgproc.warpPerspective(tempDstMat1, tempDstMat2, perspectTransfMat, tempDstMat1.size());
+        Imgproc.warpPerspective(tempSrcMat1, tempSrcMat2, perspectTransfMat, tempSrcMat1.size());
+
+        tempMatOP2f = ellipseCrawler(tempSrcMat2,(float)tempSrcMat2.width() / 2, (float)tempSrcMat2.height()/2, threshold);
+        // If anything has been null, invoke FIIOOH-protocol
+        if (tempMatOP2f == null) {
+            return new ImmutablePair<>(inputSrc, ellipse);
+        }
+
+        // Image is rotated back to original rotational orientation, and then stored in temp-Mats1
+        RotatedRect ellipseOut = Imgproc.fitEllipse(tempMatOP2f);
+        rotateM = Imgproc.getRotationMatrix2D(ellipseOut.center, - ellipse.angle, 1.0);
+        Imgproc.warpAffine(tempDstMat2, tempDstMat1, rotateM, tempDstMat2.size());
+        Imgproc.warpAffine(tempSrcMat2, tempSrcMat1, rotateM, tempSrcMat2.size());
+
+        tempMatOP2f = ellipseCrawler(tempSrcMat1,(float)tempSrcMat1.width() / 2, (float)tempSrcMat1.height()/2, threshold);
+        // Once again, if anything has been null: FIIOOH
+        if (tempMatOP2f == null) {
+            return new ImmutablePair<>(inputSrc, ellipse);
+        }
+
+        // Ellipse for further cropping is calculated.
+        ellipseOut = Imgproc.fitEllipse(tempMatOP2f);
+
+        return new ImmutablePair<>(tempDstMat1, ellipseOut);
     }
 
 }
