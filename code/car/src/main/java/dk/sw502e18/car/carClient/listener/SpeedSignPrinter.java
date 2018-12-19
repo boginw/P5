7724cd188d5e94d0fb9d18bbd5a5d272bda832b6
Bogi Napoleon Wennerstrøm <bogi.wennerstrom@gmail.com>
2018-12-19 01:23:12 +0100

    removed fifo and simplified socket stuff

--- code/car/src/main/java/dk/sw502e18/car/carClient/listener/SpeedSignPrinter.java
+++ code/car/src/main/java/dk/sw502e18/car/carClient/listener/SpeedSignPrinter.java
@@ -4,28 +4,16 @@ import dk.sw502e18.car.carClient.CarClientMessageListener;
 import lejos.hardware.BrickFinder;
 import lejos.hardware.lcd.Font;
 import lejos.hardware.lcd.GraphicsLCD;
-import lejos.hardware.lcd.Image;
-
-import java.io.IOException;
 
 public class SpeedSignPrinter implements CarClientMessageListener {
     private final GraphicsLCD g = BrickFinder.getDefault().getGraphicsLCD();
     private final int SW = g.getWidth();
     private final int SH = g.getHeight();
-    private final Image c;
-
-    public SpeedSignPrinter() throws IOException {
-        // TODO: this only works sometimes, please fix
-        // c = ImageUtils.fromResource(this, "sign.lni");
-        c = new Image(10, 10, null);
-    }
 
     @Override
     public void onMessage(String message) {
         g.setFont(Font.getFont(0, 0, Font.SIZE_LARGE));
-
         g.clear();
-        g.drawImage(c, 0, 0, 0);
 
         g.drawString(
                 message,
